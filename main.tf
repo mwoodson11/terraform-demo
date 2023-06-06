ff4d426553b1d17570cda6f42a0fd674b0472b2c
Mark Woodson <mswoodson11@gmail.com>
2023-06-05 19:15:17 -0700

    automation changes

--- main.tf
+++ main.tf
@@ -1,18 +1,27 @@
+# resource "aws_instance" "my_vm" {
+#   ami           = "ami-01d9a63b83a0d25ee" //Ubuntu AMI
+#   instance_type = "hvm:ebs-ssd"
+
+#   tags = {
+#     Name = "My EC2 instance",
+#   }
+# }
+
 provider "aws" {
   version = "~> 2.0"
   region  = "eu-central-1"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "example-terraform-project-name-bootstrap-terraform-state"
-#     key    = "default-infrastructure"
-#     region = "eu-central-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "mswoodson-example-terraform"
+    key    = "default-infrastructure"
+    region = "eu-central-1"
+  }
+}
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "example-terraform-project-name-bootstrap-terraform-state"
+  bucket = "mswoodson-example-terraform"
 
   versioning {
     enabled = true
